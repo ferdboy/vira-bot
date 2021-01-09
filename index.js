8f42b9f53e89f477011ee6ac05ead2a33299cf40
ferdboy <77190186+ferdboy@users.noreply.github.com>
2021-01-09 19:47:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 * "Jangan modal nama doang bro!!!"
 * (Scriptnya Aris187 ID).
 */
-const BotName = '🤖AR15BOT🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/_sadboy.ig'; // Nama Instagramlu cok
-const whatsapplu = 'Wa.me/+6285722553839'; // Nomor whatsapplu cok
+const BotName = '🤖F3RD1Bot🤖'; // Nama Bot Whatsapp
+const instagramlu = 'https://instagram.com/ferdi_ardian21'; // Nama Instagramlu cok
+const whatsapplu = 'Wa.me/+6282169369877'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/GU6NsgZvfgw37vrjIxUPhP'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://www.youtube.com/channel/UCGYLWtyT9IADYNUiK0uZiGg'; // OFFICIAL GRUP LU 2
-const grupch3 = 'https://instagram.com/_sadboy.ig'; // OFFICIAL GRUP LU 3
+const grupch1 = 'https://chat.whatsapp.com/GQRkuhH34C80Y6JWwG3xXl'; // OFFICIAL GRUP LU 1
+const grupch2 = 'Nothing'; // OFFICIAL GRUP LU 2
+const grupch3 = 'https://instagram.com/ferdi_ardian21'; // OFFICIAL GRUP LU 3
 //
 const qrcode = require("qrcode-terminal");
 const moment = require("moment");
@@ -53,7 +53,7 @@ conn.on('qr', qr =>
    {
       small: true
    });
-   console.log(`[ ${moment().format("HH:mm:ss")} ] AR15BOT By A187ID Ready Scan Now!`);
+   console.log(`[ ${moment().format("HH:mm:ss")} ] F3RD1Bot By Ferdi Ardian Ready Scan Now!`);
 });
 
 conn.on('credentials-updated', () =>
@@ -140,13 +140,13 @@ else if (text == 'Asu'){
 conn.sendMessage(id, 'Gx boleh kasar lu Asw' ,MessageType.text);
 }
 else if (text == '$owner'){
-conn.sendMessage(id, 'Owner wa.me/+6285733553839' ,MessageType.text);
+conn.sendMessage(id, 'Owner wa.me/+6282168369877' ,MessageType.text);
 }
 else if (text == '$creator'){
-conn.sendMessage(id, 'Creator wa.me/+6285733553839' ,MessageType.text);
+conn.sendMessage(id, 'Creator wa.me/+6282169369877' ,MessageType.text);
 }
-else if (text == 'Aris'){
-conn.sendMessage(id, 'Aku BOT nya Aris' ,MessageType.text);
+else if (text == 'Fer'){
+conn.sendMessage(id, 'Aku SYG nya Ferdi' ,MessageType.text);
 }
 else if (text == 'Ngentod'){
 conn.sendMessage(id, 'Ngentod Bapak kau_-' ,MessageType.text);
